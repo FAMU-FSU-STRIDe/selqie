d0f6f183fb0140fc44b73b3a5027de15f0e9d55e
Jonathan Boylan <jtylerboylan@outlook.com>
2024-09-04 12:13:51 -0400

    Working terminal

---
+++ robot_utils/robot_utils/odrive_util_functions.py
@@ -0,0 +1,20 @@
+from robot_msgs.msg import LegCommand, LegEstimate, MotorCommand, MotorEstimate, MotorConfig, MotorInfo
+
+def set_odrive_states(cfg_publishers, axis_state):
+    msg = MotorConfig()
+    msg.axis_state = axis_state
+    for cfg_pub in cfg_publishers:
+        cfg_pub.publish(msg)
+        
+def clear_odrive_errors(cfg_publishers):
+    msg = MotorConfig()
+    msg.clear_errors = True
+    for cfg_pub in cfg_publishers:
+        cfg_pub.publish(msg)
+        
+def set_odrive_positions(cmd_publishers, pos):
+    msg = MotorCommand()
+    msg.control_mode = MotorCommand.CONTROL_MODE_POSITION
+    msg.pos_setpoint = pos
+    for pub in cmd_publishers:
+        pub.publish(msg)
--- robot_utils/robot_utils/starq_util.py
+++ robot_utils/robot_utils/starq_util.py
@@ -1,14 +1,19 @@
 import rclpy
 from rclpy.node import Node
 from cmd import Cmd
+import os
+import threading
 
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy
+from ament_index_python.packages import get_package_share_directory
 from robot_msgs.msg import LegCommand, LegEstimate, MotorCommand, MotorEstimate, MotorConfig, MotorInfo
-from robot_utils.robot_util_functions import run_leg_trajectory_file
+from robot_utils.robot_util_functions import set_leg_states, run_leg_trajectory_file, print_motor_info, print_leg_info, get_error_name
+from robot_utils.odrive_util_functions import set_odrive_states, clear_odrive_errors, set_odrive_positions
 
 NUM_MOTORS = 8
 LEG_NAMES = ['FL', 'FR', 'RL', 'RR']
 STANDING_LEG_POSITION = [0.0, 0.0, -0.18914]
+TRAJECTORIES_FOLDER = os.path.join(get_package_share_directory('robot_utils'), 'trajectories')
 
 def qos_fast():
     return QoSProfile(
@@ -66,89 +71,91 @@ class STARQTerminal(Cmd):
     intro = 'Welcome to the STARQ terminal. Type help or ? to list commands.\n'
     prompt = 'STARQ> '
     
-    def __init__(self, robot_instance):
+    def __init__(self, robot_instance : STARQRobotNode):
         super().__init__()
         self.robot = robot_instance
 
     def do_exit(self, line):
-        """Exit the terminal."""
+        """
+        Exit the terminal.
+        """
         print("Exiting...")
+        rclpy.shutdown()
         return True
 
     def do_ready(self, line):
-        """Ready the ODrive motors"""
-        msg = MotorConfig()
-        msg.axis_state = MotorConfig.AXIS_STATE_CLOSED_LOOP_CONTROL
-        for cfg_pub in self.robot.motor_config_publishers:
-            cfg_pub.publish(msg)
+        """
+        Ready the ODrive motors
+        """
+        set_odrive_states(self.robot.motor_config_publishers, MotorConfig.AXIS_STATE_CLOSED_LOOP_CONTROL)
 
     def do_idle(self, line):
-        """Idle the ODrive motors"""
-        msg = MotorConfig()
-        msg.axis_state = MotorConfig.AXIS_STATE_IDLE
-        for cfg_pub in self.robot.motor_config_publishers:
-            cfg_pub.publish(msg)
+        """
+        Idle the ODrive motors
+        """
+        set_odrive_states(self.robot.motor_config_publishers, MotorConfig.AXIS_STATE_IDLE)
 
     def do_clear_errors(self, line):
-        """Clear errors on the ODrive motors"""
-        msg = MotorConfig()
-        msg.clear_errors = True
-        for cfg_pub in self.robot.motor_config_publishers:
-            cfg_pub.publish(msg)
+        """
+        Clear errors on the ODrive motors
+        """
+        clear_odrive_errors(self.robot.motor_config_publishers)
 
     def do_zero(self, line):
-        """Zero the ODrive motors"""
-        msg = MotorCommand()
-        msg.control_mode = MotorCommand.CONTROL_MODE_POSITION
-        msg.pos_setpoint = 0.0
-        for pub in self.robot.motor_command_publishers:
-            pub.publish(msg)
+        """
+        Zero the ODrive motors
+        """
+        set_odrive_positions(self.robot.motor_command_publishers, 0.0)
 
     def do_stand(self, line):
-        """Stand the robot"""
-        msg = LegCommand()
-        msg.control_mode = MotorCommand.CONTROL_MODE_POSITION
-        msg.pos_setpoint.x = STANDING_LEG_POSITION[0]
-        msg.pos_setpoint.y = STANDING_LEG_POSITION[1]
-        msg.pos_setpoint.z = STANDING_LEG_POSITION[2]
-        for pub in self.robot.leg_command_publishers:
-            pub.publish(msg)
+        """
+        Stand the robot
+        """
+        set_leg_states(self.robot.leg_command_publishers, MotorCommand.CONTROL_MODE_POSITION, STANDING_LEG_POSITION)
 
     def do_set_leg_position(self, line):
-        """Set the position of a leg"""
+        """
+        Set the position of a leg
+        Usage: set_leg_position <leg_name> <x> <y> <z>
+        Note: Use leg name '*' to set position for all legs
+        """
         args = line.split()
         if len(args) != 4:
             print("Invalid number of arguments")
             return
         
         leg = args[0]
-        if leg not in LEG_NAMES:
+        if leg == "*":
+            cmd_publishers = self.robot.leg_command_publishers
+        elif leg in LEG_NAMES:
+            cmd_publishers = [self.robot.leg_command_publishers[LEG_NAMES.index(leg)]]
+        else:
             print("Invalid leg name")
             return
     
         try:
-            x = float(args[1])
-            y = float(args[2])
-            z = float(args[3])
+            pos = [float(args[1]), float(args[2]), float(args[3])]
         except ValueError:
             print("Invalid position values")
             return
         
-        msg = LegCommand()
-        msg.control_mode = MotorCommand.CONTROL_MODE_POSITION
-        msg.pos_setpoint.x = x
-        msg.pos_setpoint.y = y
-        msg.pos_setpoint.z = z
-        self.robot.leg_command_publishers[LEG_NAMES.index(leg)].publish(msg)
+        set_leg_states(cmd_publishers, MotorCommand.CONTROL_MODE_POSITION, pos)
 
     def do_run_trajectory(self, line):
-        """Run a trajectory file"""
+        """
+        Run a trajectory file
+        Usage: run_trajectory <file> <num_loops> <frequency>
+        """
         args = line.split()
         if len(args) != 3:
             print("Invalid number of arguments")
             return
         
-        file = args[0]
+        file = os.path.join(TRAJECTORIES_FOLDER, args[0])
+        if not os.path.exists(file):
+            print('File ' + file + ' does not exist')
+            return
+    
         try:
             num_loops = int(args[1])
             frequency = float(args[2])
@@ -156,26 +163,50 @@ class STARQTerminal(Cmd):
             print("Invalid number of loops or frequency")
             return
 
-        run_leg_trajectory_file(file, num_loops, frequency, self.robot.leg_command_publishers)
+        run_leg_trajectory_file(self.robot.leg_command_publishers, file, num_loops, frequency)
+        
+    def complete_run_trajectory(self, text, line, begidx, endidx):
+        if len(line.split()) <= 2:
+            files = os.listdir(TRAJECTORIES_FOLDER)
+            return [f for f in files if f.startswith(text)]
+        return []
 
     def do_print_motor_info(self, line):
-        """Print motor info"""
+        """
+        Print motor info
+        """
         for i in range(NUM_MOTORS):
-            print(f"Motor {i}: {self.robot.motor_infos[i]}")
-            print(f"Motor {i}: {self.robot.motor_estimates[i]}")
+            print(f"Motor {i}:")
+            print_motor_info(self.robot.motor_infos[i], self.robot.motor_estimates[i])
 
     def do_print_leg_info(self, line):
-        """Print leg info"""
+        """
+        Print leg info
+        """
         for i in range(len(LEG_NAMES)):
-            print(f"Leg {LEG_NAMES[i]}: {self.robot.leg_estimates[i]}")
+            print(f"Leg {LEG_NAMES[i]}:")
+            print_leg_info(self.robot.leg_estimates[i])
+            
+    def do_print_errors(self, line):
+        """
+        Print all motor errors
+        """
+        iserr = False
+        for i in range(NUM_MOTORS):
+            if self.robot.motor_infos[i].axis_error != 0:
+                iserr = True
+                print(f"Error on Motor {i}: " + get_error_name(self.robot.motor_infos[i]))
+        if not iserr:
+            print("No errors on all motors")
 
 def main():
     rclpy.init()
     robot = STARQRobotNode()
+    spin_thread = threading.Thread(target=rclpy.spin, args=(robot,))
+    spin_thread.start()
     STARQTerminal(robot).cmdloop()
-    rclpy.spin(robot)
+    spin_thread.join()
     robot.destroy_node()
-    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
