0aed641d1d95fd62f9f45774313b455fb75d32b5
Jonathan Boylan <jtylerboylan@outlook.com>
2024-11-01 12:33:39 -0400

    install

--- tools/install.sh
+++ tools/install.sh
@@ -3,14 +3,25 @@
 # Update the system
 sudo apt update && sudo apt upgrade -y
 
-# Install ROS2 Humble
-sudo apt install software-properties-common
+# Install system dependencies
+sudo apt install -y software-properties-common
 sudo add-apt-repository universe
-sudo apt update && sudo apt install curl -y
+sudo apt update
+sudo apt install -y curl wget gpg git apt-transport-https
+
+# Install ROS2 Humble
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update && sudo apt upgrade -y
 sudo apt install ros-humble-desktop
+
+# Install VSCode
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+sudo apt-get update
+sudo apt-get install code
 
 # Install ROS2 dependencies
 
