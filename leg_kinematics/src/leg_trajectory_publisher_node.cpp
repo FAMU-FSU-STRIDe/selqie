252fee13ed74c1c8ff4d970483f3c7a11e8d60f7
Jonathan Boylan <jtylerboylan@outlook.com>
2025-01-14 23:55:37 -0500

    improve leg command publisher

--- leg_kinematics/src/leg_trajectory_publisher_node.cpp
+++ leg_kinematics/src/leg_trajectory_publisher_node.cpp
@@ -2,8 +2,6 @@
 #include <robot_msgs/msg/leg_command.hpp>
 #include <robot_msgs/msg/leg_trajectory.hpp>
 
-#include <thread>
-
 static inline rclcpp::QoS qos_fast()
 {
     return rclcpp::QoS(rclcpp::KeepLast(10)).best_effort();
@@ -19,28 +17,35 @@ using namespace robot_msgs::msg;
 class LegTrajectoryPublisherNode : public rclcpp::Node
 {
 private:
-    LegTrajectory::SharedPtr _traj;
-    bool _update = false;
-
     rclcpp::Subscription<LegTrajectory>::SharedPtr _leg_trajectory_sub;
     rclcpp::Publisher<LegCommand>::SharedPtr _leg_command_pub;
+    rclcpp::TimerBase::SharedPtr _publish_timer;
 
-    std::mutex _mutex;
+    std::size_t _idx = 0;
+    LegTrajectory::SharedPtr _traj;
+    rclcpp::Time _start_time;
 
 public:
     LegTrajectoryPublisherNode() : rclcpp::Node("leg_trajectory_publisher")
     {
         _leg_trajectory_sub = this->create_subscription<LegTrajectory>(
             "leg/trajectory", qos_reliable(),
-            std::bind(&LegTrajectoryPublisherNode::legTrajectory, this, std::placeholders::_1));
+            std::bind(&LegTrajectoryPublisherNode::updateTrajectory, this, std::placeholders::_1));
 
         _leg_command_pub = this->create_publisher<LegCommand>("leg/command", qos_fast());
 
-        std::thread(&LegTrajectoryPublisherNode::run, this).detach();
+        _publish_timer = this->create_wall_timer(std::chrono::milliseconds(1),
+                                                 std::bind(&LegTrajectoryPublisherNode::publishLegCommand, this));
     }
 
-    void legTrajectory(const LegTrajectory::SharedPtr msg)
+    void updateTrajectory(const LegTrajectory::SharedPtr msg)
     {
+        if (msg->timing.empty())
+        {
+            _traj = nullptr;
+            return;
+        }
+
         if (msg->timing.size() != msg->commands.size())
         {
             RCLCPP_ERROR(this->get_logger(), "Trajectory size mismatch: %lu timing, %lu commands",
@@ -48,45 +53,35 @@ public:
             return;
         }
 
-        std::sort(msg->timing.begin(), msg->timing.end());
+        if (!std::is_sorted(msg->timing.begin(), msg->timing.end()))
+        {
+            RCLCPP_ERROR(this->get_logger(), "Trajectory timing not in chronological order");
+            return;
+        }
 
-        std::lock_guard<std::mutex> lock(_mutex);
+        _idx = 0;
         _traj = msg;
-        _update = true;
+        _start_time = this->now();
     }
 
-    void run()
+    void publishLegCommand()
     {
-        auto cstart = this->now();
-        LegTrajectory traj;
-        std::size_t idx = 0;
-        while (rclcpp::ok())
+        if (!_traj)
         {
-            {
-                std::lock_guard<std::mutex> lock(_mutex);
-                if (_update)
-                {
-                    traj = *_traj;
-                    idx = 0;
-                    cstart = this->now();
-                    _update = false;
-                }
-            }
+            return;
+        }
 
-            if (idx >= traj.timing.size() || traj.commands.empty())
-            {
-                this->get_clock()->sleep_for(std::chrono::milliseconds(10));
-                continue;
-            }
+        using namespace std::chrono;
+        const auto delay = milliseconds(time_t(_traj->timing[_idx] * 1E3));
+        const auto cdiff = (this->now() - _start_time).to_chrono<milliseconds>();
+        if (delay <= cdiff)
+        {
+            _leg_command_pub->publish(_traj->commands[_idx++]);
 
-            const auto delay = std::chrono::nanoseconds(time_t(traj.timing[idx] * 1E9));
-            const auto cdiff = (this->now() - cstart).to_chrono<std::chrono::nanoseconds>();
-            if (delay > cdiff)
+            if (_idx >= _traj->timing.size())
             {
-                this->get_clock()->sleep_for(delay - cdiff);
+                _traj = nullptr;
             }
-
-            _leg_command_pub->publish(traj.commands[idx++]);
         }
     }
 };
