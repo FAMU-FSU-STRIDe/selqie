4d9df02bbd8d2e19f149c9de79a2299f56aeb661
Jonathan Boylan <jtylerboylan@outlook.com>
2025-02-18 16:22:12 -0500

    Combine config files

--- selqie_ros2/launch/gaits.launch.py
+++ selqie_ros2/launch/gaits.launch.py
@@ -14,18 +14,14 @@ import os
 from ament_index_python.packages import get_package_share_directory
 PACKAGE_NAME = 'selqie_ros2'
 CONFIG_FOLDER = os.path.join(get_package_share_directory(PACKAGE_NAME), 'config')
-
-COMMAND_VELOCITY_FILTER_CONFIG = os.path.join(CONFIG_FOLDER, 'command_velocity_filter.yaml')
-WALK_CONFIG = os.path.join(CONFIG_FOLDER, 'walk_config.yaml')
-SWIM_CONFIG = os.path.join(CONFIG_FOLDER, 'swim_config.yaml')
-JUMP_CONFIG = os.path.join(CONFIG_FOLDER, 'jump_config.yaml')
+GAIT_CONFIG = os.path.join(CONFIG_FOLDER, 'gait_config.yaml')
 
 def CommandVelocityFilterNode(use_sim_time : str):
     return Node(
         package='stride_maker',
         executable='command_velocity_filter_node',
         name='command_velocity_filter_node',
-        parameters=[COMMAND_VELOCITY_FILTER_CONFIG, {'use_sim_time': use_sim_time}]
+        parameters=[GAIT_CONFIG, {'use_sim_time': use_sim_time}]
     )
 
 def WalkNode(use_sim_time : str):
@@ -33,7 +29,7 @@ def WalkNode(use_sim_time : str):
         package='stride_maker',
         executable='walk_node',
         name='walk_node',
-        parameters=[WALK_CONFIG, {'use_sim_time': use_sim_time}],
+        parameters=[GAIT_CONFIG, {'use_sim_time': use_sim_time}],
         # prefix=['xterm -e gdb -ex run --args']
     )
     
@@ -42,7 +38,7 @@ def SwimNode(use_sim_time : str):
         package='stride_maker',
         executable='swim_node',
         name='swim_node',
-        parameters=[SWIM_CONFIG, {'use_sim_time': use_sim_time}],
+        parameters=[GAIT_CONFIG, {'use_sim_time': use_sim_time}],
     )
 
 def JumpNode(use_sim_time : str):
@@ -50,7 +46,7 @@ def JumpNode(use_sim_time : str):
         package='stride_maker',
         executable='jump_node',
         name='jump_node',
-        parameters=[JUMP_CONFIG, {'use_sim_time': use_sim_time}],
+        parameters=[GAIT_CONFIG, {'use_sim_time': use_sim_time}],
     )
 
 def SinkNode(use_sim_time : str):
@@ -58,7 +54,7 @@ def SinkNode(use_sim_time : str):
         package='stride_maker',
         executable='sink_node',
         name='sink_node',
-        parameters=[{'use_sim_time': use_sim_time}],
+        parameters=[GAIT_CONFIG, {'use_sim_time': use_sim_time}],
     )
 
 def StandNode(use_sim_time : str):
@@ -66,7 +62,7 @@ def StandNode(use_sim_time : str):
         package='stride_maker',
         executable='stand_node',
         name='stand_node',
-        parameters=[{'use_sim_time': use_sim_time}],
+        parameters=[GAIT_CONFIG, {'use_sim_time': use_sim_time}],
     )
 
 def generate_launch_description():
